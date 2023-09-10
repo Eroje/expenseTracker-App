e341eac3333250bef7b6ec4e1d324830a6c910c6
Mojid Eroje <majidy287@gmail.com>
2023-09-10 20:23:17 +0100

    fourth commit

--- expense-tracker-app/src/App.js
+++ expense-tracker-app/src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Expense Tracker</h1>
+        <h1>Eromoy Contractors Expense Tracker</h1>
         <Budgets />
         <Transactions />
       </header>
